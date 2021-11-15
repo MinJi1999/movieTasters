c6f8ddf3437b88774a678b89bff728f3e51886b5
Min Ji <kmj@uno-solution.com>
2021-11-15 23:09:26 +0900

    update: 라우트 수정 및 getMyPost 라우트 추가

--- routes/post.js
+++ routes/post.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const { Post } = require("../models/Post");
 const { auth } = require("../middleware/auth");
 
-router.post("/savePost", (req, res) => {
+router.post("/save", (req, res) => {
   const post = new Post(req.body);
   post.save((err) => {
     if (err) {
@@ -14,8 +14,29 @@ router.post("/savePost", (req, res) => {
   });
 });
 
-router.get("/getPost", (req, res) => {
+router.get("/get", (req, res) => {
   Post.find()
+    .sort({ _id: -1 })
+    .populate("writer")
+    .exec((err, postInfo) => {
+      if (err) {
+        return res.status(400).json({ success: false, err });
+      } else {
+        return res.status(200).json({ success: true, postInfo });
+      }
+    });
+});
+
+router.get("/getMyPost", (req, res) => {
+  const type = req.query.type;
+  const userId = req.query.id;
+  if (type === "array") {
+    let ids = req.query.id.split(",");
+    productIds = ids.map((item) => {
+      return item;
+    });
+  }
+  Post.find({ writer: { $in: userId } })
     .sort({ _id: -1 })
     .populate("writer")
     .exec((err, postInfo) => {
@@ -30,14 +51,12 @@ router.get("/getPost", (req, res) => {
 router.get("/post_by_id", (req, res) => {
   const type = req.query.type;
   let postId = req.query.id;
-
   if (type === "array") {
     const ids = req.query.id.split(",");
     postId = ids.map((i) => {
       return item;
     });
   }
-
   // Post.find({_id: {$in: productIds }})
   Post.find({ _id: postId })
     .populate("writer")
@@ -49,5 +68,7 @@ router.get("/post_by_id", (req, res) => {
       }
     });
 });
+
+router.put("/update", (req, res) => {});
 
 module.exports = router;
