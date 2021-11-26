b27ff96097ae2b28f441b248b737adecd82544f6
Min Ji <didtjdrla@gmail.com>
2021-11-26 23:03:00 +0900

    wip

--- index.js
+++ index.js
@@ -17,6 +17,21 @@ app.use(cookieParser());
 app.use("/api/user", require("./routes/user"));
 app.use("/api/post", require("./routes/post"));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(
+    cors({
+      origin: "https://movie-tasters.herokuapp.com/",
+      credentials: true,
+    })
+  );
+  app.use(express.static(path.join(__dirname, "client/build")));
+
+  // index.html for all page routes    html or routing and naviagtion
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./client", "build", "index.html"));
+  });
+}
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
