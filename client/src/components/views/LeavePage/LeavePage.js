8487016b46dad32ebc2e00306a9b78ecd2c54715
Min Ji <didtjdrla@gmail.com>
2021-12-22 02:12:11 +0900

    feat: Main 페이지에 진입하지 않을 시 나오는 페이지

---
+++ client/src/components/views/LeavePage/LeavePage.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./index.scss";
+
+function LeavePage() {
+  return (
+    <div className="page-bg">
+      <div className="page-title">
+        페이지에 찾아와주셔서 감사했습니다.
+        <br />
+        기회가 된다면 다음엔 더 많은 영화들과 함께 만나요!
+        <br />
+        항상 유익한 시간 보낼 수 있는 영화 미식가들이 되겠습니다.
+        <br />
+      </div>
+    </div>
+  );
+}
+
+export default LeavePage;
